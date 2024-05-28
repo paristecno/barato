fcee321a253feba83f5753525257477952e04e5e
paristecno <paristecnoargentina@gmail.com>
2024-05-28 20:20:35 -0300

    será

---
+++ src/components/Hola.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hola = () => {
+  return (
+    <div className='flex items-center justify-center py-72'>
+        <h1 className='text-4xl text-red-600 font-bold'>Hola</h1>
+    </div>
+  )
+}
+
+export default Hola
