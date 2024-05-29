89a2a85af6d058aff628fc265afa13cb945f9cb0
paristecno <paristecnoargentina@gmail.com>
2024-05-29 11:30:40 -0300

    asd

--- src/components/Hola.jsx
+++ src/components/Hola.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hola = () => {
   return (
     <div className='flex items-center justify-center py-72'>
-        <h1 className='text-4xl text-red-600 font-bold'>Hola de nuevo</h1>
+        <h1 className='text-4xl text-red-600 font-bold'>Hola de again</h1>
     </div>
   )
 }
